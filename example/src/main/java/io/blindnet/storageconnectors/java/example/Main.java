0b521bb42b6e3a8c8cbaa62c6b11ab1055f8313b
Amaury Rousseau <kinrar@kinrar.io>
2022-08-23 14:03:50 +0200

    Example: use default endpoint

--- example/src/main/java/io/blindnet/storageconnectors/java/example/Main.java
+++ example/src/main/java/io/blindnet/storageconnectors/java/example/Main.java
@@ -4,13 +4,11 @@ import io.blindnet.storageconnectors.java.StorageConnector;
 import io.blindnet.storageconnectors.java.datarequests.reply.BinaryData;
 import io.blindnet.storageconnectors.java.handlers.mapping.MappingRequestHandler;
 
-import java.net.URISyntaxException;
-
 public class Main {
     private static final User DEMO_USER = new User("John Doe");
 
-    public static void main(String[] args) throws URISyntaxException {
-        StorageConnector.create("http://localhost:8028")
+    public static void main(String[] args) {
+        StorageConnector.create()
                 .setDataRequestHandler(new MappingRequestHandler.Builder<User>()
                         .setSubjectMapper(id -> DEMO_USER)
                         .addSelectorType("NAME", User::getName)
