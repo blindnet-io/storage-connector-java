83bef0eb98b15ca77149b2bd53cc175af101162e
Amaury Rousseau <kinrar@kinrar.io>
2022-11-14 11:00:41 +0100

    Add DataAccessClient setters in ConnectorRunner

--- core/src/main/java/io/blindnet/storageconnector/ConnectorRunner.java
+++ core/src/main/java/io/blindnet/storageconnector/ConnectorRunner.java
@@ -1,9 +1,37 @@
 package io.blindnet.storageconnector;
 
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import io.blindnet.storageconnector.handlers.ErrorHandler;
+
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 
 public interface ConnectorRunner {
     DataAccessClient getDataAccessClient();
+
+    default ErrorHandler getErrorHandler() {
+        return getDataAccessClient().getErrorHandler();
+    };
+
+    default DataAccessClient setErrorHandler(ErrorHandler errorHandler) {
+        getDataAccessClient().setErrorHandler(errorHandler);
+    }
+
+    default ExecutorService getExecutorService() {
+        return getDataAccessClient().getExecutorService();
+    }
+
+    default DataAccessClient setExecutorService(ExecutorService executorService) {
+        getDataAccessClient().setExecutorService(executorService);
+    }
+
+    default JsonMapper getJsonMapper() {
+        return getDataAccessClient().getJsonMapper();
+    }
+
+    default DataAccessClient setJsonMapper(JsonMapper objectMapper) {
+        getDataAccessClient().setJsonMapper(objectMapper);
+    }
 
     default void start() {
         getDataAccessClient().getWebSocketClient().connect();
